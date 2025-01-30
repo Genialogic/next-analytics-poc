be38d1acb389a387e946b0706999db6f792d7d08
luigenia <luiggi.santos@genialogic.com.br>
2025-01-29 22:00:49 -0300

    style

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -4,5 +4,4 @@ import { sendGTMEvent } from "@next/third-parties/google";
 
 export const trackEvent = (event: string, value?: string) => {
   sendGTMEvent({ event, value });
-  console.log(`Evento enviado: ${event}, Valor: ${value || "N/A"}`);
 };
